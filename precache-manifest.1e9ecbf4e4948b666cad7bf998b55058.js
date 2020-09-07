79dbbe72be0064cec9e448b7c5f525009dd9681b
IkramMissaoui <missaoui.ikram726@gmail.com>
2020-09-07 19:43:58 +0200

    Updates

---
+++ precache-manifest.1e9ecbf4e4948b666cad7bf998b55058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02849102ba5126f1a30a6e2d00c484b0",
+    "url": "/weatherAPI/index.html"
+  },
+  {
+    "revision": "567d85130fd231cd2312",
+    "url": "/weatherAPI/static/css/main.0a58ceaa.chunk.css"
+  },
+  {
+    "revision": "95d2e2ded46d9b7b63b5",
+    "url": "/weatherAPI/static/js/2.c342cdb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherAPI/static/js/2.c342cdb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567d85130fd231cd2312",
+    "url": "/weatherAPI/static/js/main.6807ee28.chunk.js"
+  },
+  {
+    "revision": "c3c03b1318c74cf18f8f",
+    "url": "/weatherAPI/static/js/runtime-main.b8aee0c0.js"
+  }
+]);
